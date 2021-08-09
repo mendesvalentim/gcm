e38c6f3ff1238db0eb0b9bd129c8c6e2cac7bb70
Bruno <mendesvalentim@gmail.com>
2021-08-09 11:15:10 -0300

    Correnção na nomenclatura

---
+++ ocorrencias-app/src/app/ocorrencias/ocorrencias-lista/ocorrencias-lista.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ocorrencias-lista',
+  templateUrl: './ocorrencias-lista.component.html',
+  styleUrls: ['./ocorrencias-lista.component.css']
+})
+export class OcorrenciasListaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- ocorrencias-app/src/app/ocorrencias/ocorrencias.module.ts
+++ ocorrencias-app/src/app/ocorrencias/ocorrencias.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms'
 import { OcorrenciasRoutingModule } from './ocorrencias-routing.module';
 import { OcorrenciasformComponent } from './ocorrencias-form/ocorrencias-form.component';
 import { OcorrenciaslistaComponent } from './ocorrenciaslista/ocorrenciaslista.component';
+import { OcorrenciasListaComponent } from './ocorrencias-lista/ocorrencias-lista.component';
 
 
 @NgModule({
   declarations: [
     OcorrenciasformComponent,
-    OcorrenciaslistaComponent
+    OcorrenciaslistaComponent,
+    OcorrenciasListaComponent
   ],
   imports: [
     CommonModule,
