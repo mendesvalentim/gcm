307dfa475e867ab322cc83f1bccec6704ec6b7a5
Bruno <mendesvalentim@gmail.com>
2021-08-09 11:23:14 -0300

    Rodando

--- ocorrencias-app/src/app/ocorrencias.service.ts
+++ ocorrencias-app/src/app/ocorrencias.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Ocorrencia } from './ocorrencias/ocorrencias';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -19,29 +19,8 @@ export class OcorrenciasService {
      return this.http.post<Ocorrencia>('http://localhost:8080/api/ocorrencias', ocorrencia);
    }
 
-  getOcorrencia() : Ocorrencia{
-    let ocorrencia: Ocorrencia = new Ocorrencia;
-    
-    //ocorrencia.id = 10;
-    ocorrencia.idUsuario = 10;
-    ocorrencia.dataOcorrencia = '12/03/2021';
-    ocorrencia.codOcorrencia = 'm-01';
-    ocorrencia.viatura = '94127';
-    ocorrencia.numeroTalao = 123456;
-    ocorrencia.boGcm = 11213;
-    ocorrencia.equipe = 'A';
-    ocorrencia.dataCadastro = '12563';
-    ocorrencia.codOcorrencia = '12563';
-    ocorrencia.endereco = '12563';
-    ocorrencia.motorista = '12563';
-    ocorrencia.kmInicial = '12563';
-    ocorrencia.kmFinal = '12563';
-    //ocorrencia.horaInicial =
-    //ocorrencia.horaFinal = 12:53;
-    ocorrencia.auxiliar1 = '12563';
-    ocorrencia.encarregadoVtr = 'valentim';
-    ocorrencia.obs = 'asdçljlkdaslkjdsalkjlkdjlkjdlkajsdla';
-
-    return ocorrencia;
+  getOcorrencias() : Observable<Ocorrencia[]> {
+    return EMPTY;
+ 
   }
 }
