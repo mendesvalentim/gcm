37ab21d3cd3226c5e4749057681fed43a5e9a29f
Bruno <mendesvalentim@gmail.com>
2021-08-19 15:00:37 -0300

    Adicionado rota

--- ocorrencias-app/src/environments/environment.ts
+++ ocorrencias-app/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrlBase: 'http://localhost:8080',
+  clientId: 'my-angular-app'
 };
 
 /*
